e1fa1b2bd2b29e1d87c9ac68d351e73030b41fce
Yosuke Torii <jinjorweb@gmail.com>
2023-09-17 01:36:45 +0900

    file tests

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,8 +1,10 @@
-import { expect, test, afterEach } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { vi, expect, test, afterEach } from "vitest";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { App } from "./App";
 import { getMountCount, getTotalRenderCount, resetCount } from "../../counter";
 import userEvent from "@testing-library/user-event";
+import fs from "node:fs/promises";
+import { setTimeout } from "timers/promises";
 
 afterEach(() => {
   resetCount();
@@ -73,42 +75,159 @@ test("should update Max Note", async () => {
 test("should update Midi Offset", async () => {
   const user = userEvent.setup();
   render(<App />);
+  resetCount();
   const input = screen.getByLabelText(/Midi Offset/i);
   input.focus();
   await user.dblClick(input);
   await user.keyboard("{backspace}");
   await user.type(input, "42");
   expect(input).toHaveValue(42);
-  expect(getMountCount("App")).toBe(1);
-  expect(getTotalRenderCount("App")).toBe(1);
-  expect(getMountCount("Player")).toBe(1);
-  expect(getTotalRenderCount("Player")).toBe(4);
-  expect(getMountCount("Properties")).toBe(1);
-  expect(getTotalRenderCount("Properties")).toBe(4);
+  expect(getMountCount("App")).toBe(0);
+  expect(getTotalRenderCount("App")).toBe(0);
+  expect(getMountCount("Player")).toBe(0);
+  expect(getTotalRenderCount("Player")).toBe(2);
+  expect(getMountCount("Properties")).toBe(0);
+  expect(getTotalRenderCount("Properties")).toBe(2);
 });
+
 test("should update Audio Offset", async () => {
   const user = userEvent.setup();
   render(<App />);
+  resetCount();
   const input = screen.getByLabelText(/Audio Offset/i);
   input.focus();
   await user.dblClick(input);
   await user.keyboard("{backspace}");
   await user.type(input, "42");
   expect(input).toHaveValue(42);
-  expect(getMountCount("App")).toBe(1);
+  expect(getMountCount("App")).toBe(0);
+  expect(getTotalRenderCount("App")).toBe(0);
+  expect(getMountCount("MidiLoader")).toBe(0);
+  expect(getTotalRenderCount("MidiLoader")).toBe(0);
+  expect(getMountCount("AudioLoader")).toBe(0);
+  expect(getTotalRenderCount("AudioLoader")).toBe(0);
+  expect(getMountCount("ImageLoader")).toBe(0);
+  expect(getTotalRenderCount("ImageLoader")).toBe(0);
+  expect(getMountCount("Tracks")).toBe(0);
+  expect(getTotalRenderCount("Tracks")).toBe(0);
+  expect(getMountCount("Player")).toBe(0);
+  expect(getTotalRenderCount("Player")).toBe(2);
+  expect(getMountCount("Properties")).toBe(0);
+  expect(getTotalRenderCount("Properties")).toBe(2);
+  expect(getMountCount("NumberInput")).toBe(0);
+  expect(getTotalRenderCount("NumberInput")).toBe(8);
+});
+
+test("should load MIDI file", async () => {
+  render(<App />);
+  resetCount();
+  const input = screen.getByLabelText(/MIDI:/i);
+  const buffer = await fs.readFile("./src/assets/1.midi");
+  const file = new File([buffer], "1.midi", {
+    type: "audio/midi",
+  });
+  fireEvent.change(input, {
+    target: {
+      files: [file],
+    },
+  });
+  await waitFor(() => setTimeout(100));
+  expect(getMountCount("App")).toBe(0);
   expect(getTotalRenderCount("App")).toBe(1);
-  expect(getMountCount("MidiLoader")).toBe(1);
+  expect(getMountCount("MidiLoader")).toBe(0);
   expect(getTotalRenderCount("MidiLoader")).toBe(1);
-  expect(getMountCount("AudioLoader")).toBe(1);
+  expect(getMountCount("AudioLoader")).toBe(0);
   expect(getTotalRenderCount("AudioLoader")).toBe(1);
-  expect(getMountCount("ImageLoader")).toBe(1);
+  expect(getMountCount("ImageLoader")).toBe(0);
   expect(getTotalRenderCount("ImageLoader")).toBe(1);
-  expect(getMountCount("Tracks")).toBe(1);
+  expect(getMountCount("Tracks")).toBe(0);
   expect(getTotalRenderCount("Tracks")).toBe(1);
-  expect(getMountCount("Player")).toBe(1);
-  expect(getTotalRenderCount("Player")).toBe(4);
-  expect(getMountCount("Properties")).toBe(1);
-  expect(getTotalRenderCount("Properties")).toBe(4);
-  expect(getMountCount("NumberInput")).toBe(4);
-  expect(getTotalRenderCount("NumberInput")).toBe(12);
+  expect(getMountCount("Player")).toBe(0);
+  expect(getTotalRenderCount("Player")).toBe(1);
+  expect(getMountCount("Properties")).toBe(0);
+  expect(getTotalRenderCount("Properties")).toBe(1);
+  expect(getMountCount("NumberInput")).toBe(0);
+  expect(getTotalRenderCount("NumberInput")).toBe(4);
+});
+
+test("should load Image file", async () => {
+  const file = new File([""], "test.png", {
+    type: "image/png",
+  });
+  vi.stubGlobal("URL", {
+    createObjectURL: () => "",
+    revokeObjectURL: vi.fn(),
+  });
+  vi.stubGlobal(
+    "Image",
+    vi.fn(() => ({
+      onload: vi.fn(),
+      set src(_: string) {
+        this.onload();
+      },
+      naturalWidth: 640,
+      naturalHeight: 384,
+    }))
+  );
+  render(<App />);
+  resetCount();
+  const input = screen.getByLabelText(/Image:/i);
+  fireEvent.change(input, {
+    target: {
+      files: [file],
+    },
+  });
+  await waitFor(() => setTimeout(100));
+  expect(getMountCount("App")).toBe(0);
+  expect(getTotalRenderCount("App")).toBe(1);
+  expect(getMountCount("MidiLoader")).toBe(0);
+  expect(getTotalRenderCount("MidiLoader")).toBe(1);
+  expect(getMountCount("AudioLoader")).toBe(0);
+  expect(getTotalRenderCount("AudioLoader")).toBe(1);
+  expect(getMountCount("ImageLoader")).toBe(0);
+  expect(getTotalRenderCount("ImageLoader")).toBe(1);
+  expect(getMountCount("Tracks")).toBe(0);
+  expect(getTotalRenderCount("Tracks")).toBe(1);
+  expect(getMountCount("Player")).toBe(0);
+  expect(getTotalRenderCount("Player")).toBe(1);
+  expect(getMountCount("Properties")).toBe(0);
+  expect(getTotalRenderCount("Properties")).toBe(1);
+  expect(getMountCount("NumberInput")).toBe(0);
+  expect(getTotalRenderCount("NumberInput")).toBe(4);
+});
+
+test("should load Wave file", async () => {
+  const AudioContextMock = vi.fn(() => ({
+    decodeAudioData: () => ({}),
+  }));
+  vi.stubGlobal("AudioContext", AudioContextMock);
+
+  render(<App />);
+  resetCount();
+  const input = screen.getByLabelText(/Audio:/i);
+  const file = new File([""], "test.wav", {
+    type: "audio/wav",
+  });
+  fireEvent.change(input, {
+    target: {
+      files: [file],
+    },
+  });
+  await waitFor(() => setTimeout(100));
+  expect(getMountCount("App")).toBe(0);
+  expect(getTotalRenderCount("App")).toBe(1);
+  expect(getMountCount("MidiLoader")).toBe(0);
+  expect(getTotalRenderCount("MidiLoader")).toBe(1);
+  expect(getMountCount("AudioLoader")).toBe(0);
+  expect(getTotalRenderCount("AudioLoader")).toBe(1);
+  expect(getMountCount("ImageLoader")).toBe(0);
+  expect(getTotalRenderCount("ImageLoader")).toBe(1);
+  expect(getMountCount("Tracks")).toBe(0);
+  expect(getTotalRenderCount("Tracks")).toBe(1);
+  expect(getMountCount("Player")).toBe(0);
+  expect(getTotalRenderCount("Player")).toBe(1);
+  expect(getMountCount("Properties")).toBe(0);
+  expect(getTotalRenderCount("Properties")).toBe(1);
+  expect(getMountCount("NumberInput")).toBe(0);
+  expect(getTotalRenderCount("NumberInput")).toBe(4);
 });
--- src/components/ImageLoader.tsx
+++ src/components/ImageLoader.tsx
@@ -11,12 +11,9 @@ export const ImageLoader = ({ onLoad }: Props) => {
   const [size, setSize] = useState<Size | null>(null);
   const handleLoadImage = (file: File) => {
     void (async () => {
-      const buffer = await file.arrayBuffer();
-      const size = await getImageSize(file);
-      const blob = new Blob([buffer], { type: file.type });
-      const url = URL.createObjectURL(blob);
-      setSize(size);
-      onLoad({ url, size });
+      const image = await getImageInfo(file);
+      setSize(image.size);
+      onLoad(image);
     })();
   };
   return (
@@ -34,24 +31,22 @@ export const ImageLoader = ({ onLoad }: Props) => {
     </label>
   );
 };
-async function getImageSize(file: File): Promise<Size> {
+async function getImageInfo(file: File): Promise<Image> {
+  const buffer = await file.arrayBuffer();
+  const blob = new Blob([buffer], { type: file.type });
   return new Promise((resolve, reject) => {
     const img = new window.Image();
-
     img.onload = () => {
       const size = {
         width: img.naturalWidth,
         height: img.naturalHeight,
       };
-
       URL.revokeObjectURL(img.src);
-      resolve(size);
+      resolve({ size, url: URL.createObjectURL(blob) });
     };
-
     img.onerror = (error) => {
       reject(error);
     };
-
     img.src = URL.createObjectURL(file);
   });
 }
