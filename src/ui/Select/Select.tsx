1cba414bcdd1c6f195e4145540dfd36d42b6d51a
Yosuke Torii <jinjorweb@gmail.com>
2023-11-04 15:12:09 +0900

    Better select (#48)
    * Add Select component to Properties and update CSS
    
    * better style for select

---
+++ src/ui/Select/Select.tsx
@@ -0,0 +1,32 @@
+import { useCounter } from "../../counter";
+
+export const Select = ({
+  value,
+  options,
+  onChange,
+}: {
+  value: string;
+  options: string[];
+  onChange: (value: string) => void;
+}) => {
+  useCounter("Select");
+  return (
+    <div className="select">
+      <select
+        value={value}
+        onChange={(e) => {
+          onChange(e.target.value);
+        }}
+      >
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <svg viewBox="-6 -9 24 24">
+        <path d="M 0 0 L 6 6 L 12 0" fill="currentColor"></path>
+      </svg>
+    </div>
+  );
+};
