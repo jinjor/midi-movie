3879fbd8b9602ffa280085fbd24031ccab6738ec
Yosuke Torii <jinjorweb@gmail.com>
2023-11-04 15:10:05 +0900

    better style for select

--- src/ui/Select/Select.tsx
+++ src/ui/Select/Select.tsx
@@ -11,17 +11,22 @@ export const Select = ({
 }) => {
   useCounter("Select");
   return (
-    <select
-      value={value}
-      onChange={(e) => {
-        onChange(e.target.value);
-      }}
-    >
-      {options.map((option) => (
-        <option key={option} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
+    <div className="select">
+      <select
+        value={value}
+        onChange={(e) => {
+          onChange(e.target.value);
+        }}
+      >
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <svg viewBox="-6 -9 24 24">
+        <path d="M 0 0 L 6 6 L 12 0" fill="currentColor"></path>
+      </svg>
+    </div>
   );
 };
