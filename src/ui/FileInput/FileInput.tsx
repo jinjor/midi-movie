761522c490b1386709e942bacb5fa07a19653360
Yosuke Torii <jinjorweb@gmail.com>
2023-11-26 03:28:23 +0900

    new layout

---
+++ src/ui/FileInput/FileInput.tsx
@@ -0,0 +1,27 @@
+import { ChangeEvent } from "react";
+import styles from "./FileInput.module.css";
+import { cx } from "@/util";
+
+type Props = {
+  onLoad: (file: File) => void;
+  extensions: string[];
+  children?: React.ReactNode;
+};
+export const FileInput = ({ onLoad, extensions, children }: Props) => {
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+    onLoad(file);
+  };
+  console.log(typeof children);
+  return (
+    <div className={cx("button", styles.fileInput)}>
+      <input
+        type="file"
+        onChange={handleFileChange}
+        accept={extensions.join(",")}
+      />
+      {children || "Select"}
+    </div>
+  );
+};
